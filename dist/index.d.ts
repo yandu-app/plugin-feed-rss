46ba2b2a14f8f1a6dcc3c5fe4b83ba4ac762c98f
255doesnotexist <chenyejin2004@foxmail.com>
2026-05-09 18:03:55 +0800

    Add .gitignore

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare const _default: {
-    name: string;
-    version: string;
-    register(system: import("@yandu/types").KernelSystem): void;
-};
-export default _default;
-//# sourceMappingURL=index.d.ts.map
