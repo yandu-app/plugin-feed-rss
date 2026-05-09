f442df9f50123e702e5cea76dccacf63de742a0e
255doesnotexist <chenyejin2004@foxmail.com>
2026-05-09 17:57:57 +0800

    Initial commit: RSS feed adapter plugin

---
+++ README.md
@@ -0,0 +1,9 @@
+# @yandu/plugin-feed-rss
+
+Yandu plugin for generic RSS/Atom feed.
+
+## Installation
+
+```bash
+npm install @yandu/plugin-feed-rss
+```
---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+declare const _default: {
+    name: string;
+    version: string;
+    register(system: import("@yandu/types").KernelSystem): void;
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
